1ba03d6a5be9af21db0cd0067b240e99723072dd
NeeharikaRN <n.neeharikareddy@gmail.com>
2025-10-03 01:17:43 +0530

    Update transform.py

--- transform.py
+++ transform.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
+
 def clean_data(csv_path):
     df = pd.read_csv(csv_path)
     df['name'] = df['name'].str.upper()  # Convert names to uppercase
     df = df[df['age'] > 28]  # Filter out ages <= 28
     return df
+
 
 if __name__ == "__main__":
     df_cleaned = clean_data("data/sample.csv")
